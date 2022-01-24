035676ddbe72ad38b47584305841c273cae60af1
SamiThueson <wea16002@byui.edu>
2022-01-24 14:15:09 -0700

    Added code and new files

--- server.js
+++ server.js
@@ -1,11 +1,19 @@
-const express = require('express')
+const express = require('express');
 
 const port = 3000;
 
 const app = express();
 
-app.get('/', (req, res)=>{
+app.get('/', (req,res)=>{
     res.send("Hello browser");
+});
+
+app.post('/login', (req,res) =>{
+    if(req.body.userName == "samithueson"){
+        res.send("Welcome!");
+    } else{
+        res.send("Who are you?");
+    }
 });
 
 app.listen(port, ()=>{});
